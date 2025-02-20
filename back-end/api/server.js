9880885c766b9e5abf3ffa70ef5201968ba004f6
MrZeroUp <umnordestinoyt@gmail.com>
2025-02-20 10:44:16 -0300

    Ajust deploy

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -10,6 +10,9 @@
 import express from "express";
 import cors from "cors";
 import { db } from "./connect.js";
+import path from "path";
+
+const __dirname = path.resolve();
 
 const app = express();
 const PORT = 3001;
@@ -17,18 +20,23 @@ const PORT = 3001;
 app.use( cors() );
 // app.use( express.json() );
 
-app.get("/", (request, response) => {
+app.get("/api/", (request, response) => {
   response.send(" working with '/artists' and '/songs' ");
 });
 
-app.get("/artists", async (request, response) => {
+app.get("/api/artists", async (request, response) => {
   response.send( await db.collection("artists").find({}).toArray() );
 });
 
-app.get("/songs", async (request, response) => {
+app.get("/api/songs", async (request, response) => {
   response.send( await db.collection("songs").find({}).toArray() );
 });
 
+app.use( express.static( path.join(__dirname, "../front-end/dist") ) );
+
+app.get("*", async (request, response) => {
+  response.sendFile( path.join(__dirname, "../front-end/dist/index.html") );
+});
 
 app.listen(PORT, () => {
   console.log(` Server running in Port: ${PORT} `);
